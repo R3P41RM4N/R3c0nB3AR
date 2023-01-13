3550098e1cbcc5047970a5b23b035cfcc90b484f
R3P41RM4N <102443920+R3P41RM4N@users.noreply.github.com>
2023-01-13 16:40:26 -0700

    Update rb.py
    Notes for Ben: It took me a bit, but I got the application to parse our the headers as well as the CSP. Changes the colors to something more readable (Green), Labelled the headers, and basically was prettying it up. 
    
    Would love your thoughts on how we can improve it and also what other apps we should add. I did fix the output to log issue vs. what's displayed on the terminal. It's not pretty, but it works. Basically I f.write and then go back and print(). That's where I left off with it. It's connecting to burp now and I think we're close to just tweaking it for basic webapp reconaissance. 
    
    One of my friends suggested using a wordpress app as well.

--- rb.py
+++ rb.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-#note for self: import colorama, import http lib
 import os
 import re
 import string
@@ -11,9 +10,6 @@ from datetime import date
 from termcolor import colored, cprint
 from art import *
 from colorama import Fore, Back, Style
-
-#Opening for the log path
-
 art1=text2art("R3c0nB3AR v1.1", font="standard")
 art2=(colored('''MMMMMMMMMMMWMMWNKXWMMMMMMMMMMMMMMMMMMMMM
 MMMMMMMMWXOxxxl:co0MMMMMMMMMMMMMMMMMMMMM
@@ -52,6 +48,7 @@ MMMMMMNd;,;::cccc::dO00kx:':lll:;xMMMMMM
 MMMMMMMN0kxkO0KXK0Kxc;,,,,'',;,.'kMMMMMM
 MMMMMMMMMMMMMMMMMMWOoc,......'',dNMMMMMM''',"green", attrs=["bold"]))
 art3=(colored("created by r3p41rm4n and ***REMOVED***", "green", attrs=["bold"]))
+
 print(art1)
 print(art2)
 print(art3+"\n\n\n")
@@ -62,108 +59,113 @@ site = str(raw_site_input).strip()
 raw_port_input = input(colored('and which ports would you like to scan today?[separate each port by a ","]\n >>', "white"))
 port = str(raw_port_input).strip()
 
-
+#establish burp connection
 proxies = {"http":"http://127.0.0.1:8080","https": "http://127.0.0.1:8080"}
+#define r = get requests
 r = requests.get(f"https://{site}", proxies=proxies, verify=False)
-
-def format_text(title,item):
-	cr = '\r\n'
-	section_break = cr + "*"*40 + cr
-	item = str(item)
-	text = Style.BRIGHT + Fore.RED + title + Fore.RESET + section_break + item + section_break
-	return text;
-
+def format_text(title,item): #this is an autoformating feature for HTTP resposne data
+    cr = '\r\n'
+    section_break = cr + "*"*40 + cr
+    item = str(item)
+    text = Style.BRIGHT + Fore.GREEN + title + Fore.RESET + section_break + item + section_break
+    return text;
+def text_header(respd):
+    cr = '\r\n'
+    section_break = cr + "*"*40
+    respd = str(respd)
+    text = Style.BRIGHT + Fore.GREEN + cr+ respd + Fore.RESET
+    return text;
+#Opening for the log path
 logpath = f"logs/{date.isoformat(date.today()).replace('-',' ')}_{site}.log"
 with open(logpath, 'w') as f:
-
-	f.write(colored("\nInitiating Recon Scan\n\n\n", "red"))
-	f.write("the following are the results of your scan conducted on {date.isoformat(date.today()).replace('-',' ')}")
-#establish burp connection
-
- #headers Scan functions
-	headers = r.headers
-
+    f.write(colored("\nInitiating Recon Scan\n\n\n", "green"))
+    print(colored("\nInitiating Recon Scan\n\n\n", "green"))
+  #headers Scan functions
+    headers = r.headers
  #cookies scan functions
-
  #response = requests.get(f"https://{site}")
  #cookies = response.cookies
-
  #s = requests.Session()
  #r = s.get(f"https://{site}")
  #sCookies = r.cookies
-
- #scans to be performed:
-	cmds =[
-		#f"nmap -p 80,443,3389 --script http-bigip-cookie {site}",
-		#f"nmap -p 80,443,3389 --script http-headers {site}",
-		#f"nmap -p 80,443,3389 --script http-security-headers {site}",
-		#f"nmap -p 139,145 --script=smb-vuln* {site}",
-		# f"nmap -vv -p 139,145 --script=smb-server-stats.nse {site}",
-		# f"nmap -vv -p 139,145 --script=smb-brute.nse {site}",
-		#f"nmap -vv -p 139,145 --script=smb-double-pulsar-backdoor.nse {site}",
-		f"nmap -Pn -T4 --script ssl-enum-ciphers --reason {site}",
-		f"nmap -Pn -sC -T4 --reason {site}",
-		f"nmap -p {port} --script http-auth, {site}",
-		f"nmap -p {port} --script http-auth-finder, {site}",
-		f"nmap -p {port} --script http-ntlm-info {site}",
-		f"nmap -p {port} --script http-aspnet-debug {site}",
-		f"nmap -p {port} --script http-stored-xss {site}",
-		f"nmap -p {port} --script http-methods {site}",
-		f"nmap -vv -sT -A -sV --reason --script=http-enum {site}",
-		f"nmap -vv -p 139,145 --script=smb-enum* {site}",
-		f"nmap -vv -p 139,145 --script=smb-os-discovery.nse {site}",
-		f"nmap -vv -p {port} --script=http-sitemap-generator.nse {site}",
-		f"nikto -p {port} -h {site}",
-		f"hping3 -S {site} -c 15 -p {port}", #note to research this function further
-		f"curl -k https://{site}/images",
-		f"curl -k https://{site}/Images",
-		f"curl -svk https://{site}/asdf",
-		f"dirb https://{site}/ /usr/share/wordlists/SecLists/Discovery/Web_Content/Top500-RobotsDisallowed.txt -S -l",
-		f"ffuf -w /usr/share/wordlists/SecLists/Discovery/DNS/deepmagic.com_top50kprefixes.txt -u https://{site}/FUZZ -c -v -fc 404,302,301",
-		f"feroxbuster -u https://{site} --filter-status 400,404,302,301 --extract-links --auto-bail",
-		f"nmap -vv -sU -p-  {site}", 
-		f"nmap -vv -sT -p-  {site}"
-	    ]
-	        #This should be run first and the open ports used as input for the rest of the program. Then they wouldn't have to specify ports. Research how to designate results as new variables in Python.
-				
-
-	 #recording information and 'for loop'
-	f.write(format_text('r.status_code is: ',r.status_code))
-	#print(format_text('r.headers is: ',r.headers)), #expiriment with a for loop for formatting
-	f.write(format_text('r.cookies is: ',r.cookies)), #build in a session cookie request
-	f.write(format_text('r.text is: ',r.text)),
-
-	f.write(colored("\n\nHEADERS\n\n", "cyan", attrs=["bold"]))
-
-	for header in headers:
-	    if (header.upper() == 'CONTENT-SECURITY-POLICY'):
-	        csp = headers[header].split(";")
-	        f.write(format_text('Content Security Policy Headers are: ', header))
-	        for c in csp:
-	            f.write(f"\t{c}")
-	    else:
-	        f.write(f"{header} : {headers[header]}")
-	    
-
-
-
-	       # print(format_text('r.cookies is: ',r.cookies))
-	       # print(format_text('r.text is: ',r.text))
-	   #  for sCookie in sCookies:
-	       #   print('sCookie name : '+sCookie.name)
-	       #   print('sCookie value : '+sCookie.value)
-
-	  #  for cookie in resp.cookies:
-	        #  print('cookie name : ',[cookie.name](http://cookie.name/))
-	         # print('cookie value : ',cookie.value)
-
-	for cmd in cmds:
-	    f.write(f"\n\n Running: {cmd}")
-	    map = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
-	    output, err = map.communicate()
-	    output = output.decode('ascii')
-	    f.write(output)
-	    f.write(f"\n\n(Complete:,{cmd}\n,{output}\n")
-	    f.write(f"--------------------------------------")
-
-	f.write(colored('**Scan Complete**','green'))
+#scans to be performed:
+    cmds =[
+        #f"nmap -p 80,443,3389 --script http-bigip-cookie {site}",
+        #f"nmap -p 80,443,3389 --script http-headers {site}",
+        #f"nmap -p 80,443,3389 --script http-security-headers {site}",
+        #f"nmap -p 139,145 --script=smb-vuln* {site}",
+        # f"nmap -vv -p 139,145 --script=smb-server-stats.nse {site}",
+        # f"nmap -vv -p 139,145 --script=smb-brute.nse {site}",
+        #f"nmap -vv -p 139,145 --script=smb-double-pulsar-backdoor.nse {site}",
+        f"nmap -Pn -T4 --script ssl-enum-ciphers --reason {site}",
+        f"nmap -Pn -sC -T4 --reason {site}",
+        f"nmap -p {port} --script http-auth, {site}",
+        f"nmap -p {port} --script http-auth-finder, {site}",
+        f"nmap -p {port} --script http-ntlm-info {site}",
+        f"nmap -p {port} --script http-aspnet-debug {site}",
+        f"nmap -p {port} --script http-stored-xss {site}",
+        f"nmap -p {port} --script http-methods {site}",
+        f"nmap -vv -sT -A -sV --reason --script=http-enum {site}",
+        f"nmap -vv -p 139,145 --script=smb-enum* {site}",
+        f"nmap -vv -p 139,145 --script=smb-os-discovery.nse {site}",
+        f"nmap -vv -p {port} --script=http-sitemap-generator.nse {site}",
+        f"nikto -p {port} -h {site}",
+        f"hping3 -S {site} -c 15 -p {port}", #note to research this function further
+        f"curl -k https://{site}/images",
+        f"curl -k https://{site}/Images",
+        f"curl -svk https://{site}/asdf",
+        f"dirb https://{site}/ /usr/share/wordlists/SecLists/Discovery/Web_Content/Top500-RobotsDisallowed.txt -S -l",
+        f"ffuf -w /usr/share/wordlists/SecLists/Discovery/DNS/deepmagic.com_top50kprefixes.txt -u https://{site}/FUZZ -c -v -fc 404,302,301",
+        f"feroxbuster -u https://{site} --filter-status 400,404,302,301 --extract-links --auto-bail",
+        f"nmap -vv -sU -p-  {site}", 
+        f"nmap -vv -sT -p-  {site}"
+        ]
+            #Future looks like complete scan first and designate variables based on open ports 
+            #Then they wouldn't have to specify ports. Research how to designate results as new variables in Python.
+    f.write(format_text('Response status_code is: ',r.status_code))
+    print(format_text('Response status_code is: ',r.status_code))
+    #f.write(format_text('headers are: ',r.headers)), #expiriment with a for loop for formatting
+    #print(format_text('headers are: ',r.headers)), #expiriment with a for loop for formatting
+    f.write(text_header('The Headers returned are: '))
+    print(text_header('The Headers returned are: '))
+    for header in headers:
+        if (header.upper() == 'CONTENT-SECURITY-POLICY'):
+            csp = headers[header].split(";")
+            f.write(format_text('', header))
+            print(format_text('', header))
+            for c in csp:
+                f.write(f"\t{c}")
+                print(f"\t{c}")
+        else:
+            (header.upper() != 'CONTENT-SECURITY-POLICY')
+            head = headers[header].split(":")
+            f.write(format_text('', header))
+            print(format_text('', header))
+            for h in head:
+                f.write(f"\t{h}")
+                print(f"\t{h}")
+    f.write(format_text('\n\nCookies: \n',r.cookies)), #build in a session cookie request
+    print(format_text('\n\nCookies: \n',r.cookies)),
+    f.write(format_text('HTML ',r.text)),
+    print(format_text('HTML: ',r.text)),
+        #else:
+         #   f.write(f"{headers} : {headers[header]}")
+          #  print(f"{headers} : {headers[header]}")
+       #  for sCookie in sCookies:
+           #   print('sCookie name : '+sCookie.name)
+           #   print('sCookie value : '+sCookie.value)
+      #  for cookie in resp.cookies:
+            #  print('cookie name : ',[cookie.name](http://cookie.name/))
+             # print('cookie value : ',cookie.value)
+    for cmd in cmds:
+        f.write(f"\n\n Running: {cmd}")
+        map = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+        output, err = map.communicate()
+        output = output.decode('ascii')
+        f.write(output)
+        print(output)
+        print(f"\n\n(Complete:,{cmd}\n,{output}\n")
+        print(f"--------------------------------------")        
+        f.write(f"\n\n(Complete:,{cmd}\n,{output}\n")
+        f.write(f"--------------------------------------")
+    f.write(colored('**Scan Complete**','green'))

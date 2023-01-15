9d375f84ebbc4591274233fc2402cd0844b4caf8
Ben Alvarado <5432821+bwalvarado@users.noreply.github.com>
2023-01-14 18:15:46 -0600

    moved the bear
    moved logo into separate file

---
+++ bear.py
@@ -0,0 +1,44 @@
+def bear():
+    art1=text2art("R3c0nB3AR v1.1", font="standard")
+    art2=(colored('''
+    MMMMMMMMMMMWMMWNKXWMMMMMMMMMMMMMMMMMMMMM
+    MMMMMMMMWXOxxxl:co0MMMMMMMMMMMMMMMMMMMMM
+    MMMMMMWKxlccc:..'.,xKNMMMMMMMMMMMMMMMMMM
+    MMN0OOxl;:cllc,.  .';lONMMMMMMMMMMMMMMMM
+    MMO;'ccclllllllc;;ccc:cxXMMMMMMMMMMMMMMM
+    MMWOc;:;:lllllllllllllllxXMMMMMMMMMMMMMM
+    MMMWOlc;;;clllllllllllllldOXWMMMMMMMMMMM
+    MMMMMWNX0c.':clllllllllllccokKWMMMMMMMMM
+    MMMMMMMMK:..';cc;:llllllllllclxXMMMMMMMM
+    MMMMMMMM0c.'::c;,:lllllllllllcckWMMMMMMM
+    MMMMMMMWk'.'cll:';lllllllllllc:xWMMMMMMM
+    MMMMMMMWx..;lllc:;;:cclllllllcckWMMMMMMM
+    MMMMMMWO, .clllllc:;;;;cllllllckWMMMMMMM
+    MMMMMMNd. ,lllcc::c::;,;clllll:dNMMMMMMM
+    MMMMMMXl. 'ccc::cllllllcc:;;;:;:0MMMMMMM
+    MMMMW0c.  .:c::lllc;,''...    ..dNMMMMMM
+    MMW0l.     ,c::c;'"",,,,,,,,;;,.;0MMMMMM
+    MWOc:,.    ,lc;,""":ccclllllllc;'lXMMMMM
+    MNklll:.  .;llcccclllccclllllllc;,xWMMMM
+    MMNkocc;ldccllllllllllccclllllll;.cXMMMM
+    MMMWX0xkKKocllllllllcc:ccllllllc'.;0MMMM
+    MMMMMMMMMXdclllllll:;;::ccllllc;..;0MMMM
+    MMMMMMMMMWOlclllllc::cllclllllc,..lXMMMM
+    MMMMMMMMMNd,:lllllc::clllllllll:',OWMMMM
+    MMMMMMMMMNo.,clllll:,:cllllllllc:oXMMMMM
+    MMMMMMMMMMO,.,:lllc;;:clllllllllcdXMMMMM
+    MMMMMMMMMWx'...,::::::cllllllllllcxNMMMM
+    MMMMMMMMMNx;......;llc:llllllllllcoKMMMM
+    MMMMMMMMMW0o:,....'cdlcclllllllllcl0MMMM
+    MMMMMMMMMMWKdc:,...';cl:cllllllllcl0MMMM
+    MMMMMMMMMMMW0olc;....:dc:clllllllcxNMMMM
+    MMMMMMMMMMMW0ollc;.,ckXx::clllllcc0MMMMM
+    MMMMMMWXOkxxollllc;cxKMNd',clllc:xNMMMMM
+    MMMMMMNd;,;::cccc::dO00kx:':lll:;xMMMMMM
+    MMMMMMMN0kxkO0KXK0Kxc;,,,,'',;,.'kMMMMMM
+    MMMMMMMMMMMMMMMMMMWOoc,......'',dNMMMMMM''',"green", attrs=["bold"]))
+    art3=(colored("created by r3p41rm4n and bwalvarado", "green", attrs=["bold"]))
+
+    print(art1)
+    print(art2)
+    print(art3+"\n\n\n")
--- rb.py
+++ rb.py
@@ -10,7 +10,7 @@ from datetime import date
 from termcolor import colored, cprint
 from art import *
 from colorama import Fore, Back, Style
-
+from bear import bear
 
 def format_text(title,item): #this is an autoformating feature for HTTP resposne data
     cr = '\r\n'
@@ -26,52 +26,6 @@ def text_header(respd):
     respd = str(respd)
     text = Style.BRIGHT + Fore.GREEN + cr+ respd + Fore.RESET
     return text;
-
-
-def bear():
-    art1=text2art("R3c0nB3AR v1.1", font="standard")
-    art2=(colored('''
-    MMMMMMMMMMMWMMWNKXWMMMMMMMMMMMMMMMMMMMMM
-    MMMMMMMMWXOxxxl:co0MMMMMMMMMMMMMMMMMMMMM
-    MMMMMMWKxlccc:..'.,xKNMMMMMMMMMMMMMMMMMM
-    MMN0OOxl;:cllc,.  .';lONMMMMMMMMMMMMMMMM
-    MMO;'ccclllllllc;;ccc:cxXMMMMMMMMMMMMMMM
-    MMWOc;:;:lllllllllllllllxXMMMMMMMMMMMMMM
-    MMMWOlc;;;clllllllllllllldOXWMMMMMMMMMMM
-    MMMMMWNX0c.':clllllllllllccokKWMMMMMMMMM
-    MMMMMMMMK:..';cc;:llllllllllclxXMMMMMMMM
-    MMMMMMMM0c.'::c;,:lllllllllllcckWMMMMMMM
-    MMMMMMMWk'.'cll:';lllllllllllc:xWMMMMMMM
-    MMMMMMMWx..;lllc:;;:cclllllllcckWMMMMMMM
-    MMMMMMWO, .clllllc:;;;;cllllllckWMMMMMMM
-    MMMMMMNd. ,lllcc::c::;,;clllll:dNMMMMMMM
-    MMMMMMXl. 'ccc::cllllllcc:;;;:;:0MMMMMMM
-    MMMMW0c.  .:c::lllc;,''...    ..dNMMMMMM
-    MMW0l.     ,c::c;'"",,,,,,,,;;,.;0MMMMMM
-    MWOc:,.    ,lc;,""":ccclllllllc;'lXMMMMM
-    MNklll:.  .;llcccclllccclllllllc;,xWMMMM
-    MMNkocc;ldccllllllllllccclllllll;.cXMMMM
-    MMMWX0xkKKocllllllllcc:ccllllllc'.;0MMMM
-    MMMMMMMMMXdclllllll:;;::ccllllc;..;0MMMM
-    MMMMMMMMMWOlclllllc::cllclllllc,..lXMMMM
-    MMMMMMMMMNd,:lllllc::clllllllll:',OWMMMM
-    MMMMMMMMMNo.,clllll:,:cllllllllc:oXMMMMM
-    MMMMMMMMMMO,.,:lllc;;:clllllllllcdXMMMMM
-    MMMMMMMMMWx'...,::::::cllllllllllcxNMMMM
-    MMMMMMMMMNx;......;llc:llllllllllcoKMMMM
-    MMMMMMMMMW0o:,....'cdlcclllllllllcl0MMMM
-    MMMMMMMMMMWKdc:,...';cl:cllllllllcl0MMMM
-    MMMMMMMMMMMW0olc;....:dc:clllllllcxNMMMM
-    MMMMMMMMMMMW0ollc;.,ckXx::clllllcc0MMMMM
-    MMMMMMWXOkxxollllc;cxKMNd',clllc:xNMMMMM
-    MMMMMMNd;,;::cccc::dO00kx:':lll:;xMMMMMM
-    MMMMMMMN0kxkO0KXK0Kxc;,,,,'',;,.'kMMMMMM
-    MMMMMMMMMMMMMMMMMMWOoc,......'',dNMMMMMM''',"green", attrs=["bold"]))
-    art3=(colored("created by r3p41rm4n and bwalvarado", "green", attrs=["bold"]))
-
-    print(art1)
-    print(art2)
-    print(art3+"\n\n\n")
 
 
 def main():

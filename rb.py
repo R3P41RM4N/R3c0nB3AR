2ddbdb6da4c73cd8ca738b5cac875ff8c348ac10
R3P41RM4N <102443920+R3P41RM4N@users.noreply.github.com>
2023-01-20 00:02:37 -0700

    Update rb.py
    Updated interface for scanning all ports. running into issue with netmask via https so unable to scan, but the code goes through the process!

--- rb.py
+++ rb.py
@@ -27,72 +27,76 @@ def text_header(respd):
     respd = str(respd)
     text = Style.BRIGHT + Fore.GREEN + cr+ respd + Fore.RESET
     return text;
-
+        #----------------Targeted Ports----------------------
+def targetPorts():
+    raw_port_input = input(colored('which ports would you like to focus your scans on today?[separate each port by a ","]\n >>', "white"))
+    port = str(raw_port_input).strip()
 
 def main():
   
     if not os.path.exists("logs"):
         os.makedirs("logs")
-
-#-----------------Opening-------------    
-    bear()
-    
+        
     #target web address
+
+    bear()
+
     raw_site_input = input(colored('Hello!, which site would you like to scan today?[Include http:// or https://]\n >>', "white"))
     site = str(raw_site_input).strip()
+
+    #-----------------Opening-------------    
     
-    #allPorts Scan First?
     raw_allPortsQ = input(colored('Would you like to scan all ports first and then designate target ports?\n(Y/N)', "cyan"))
     allPortsQ = str(raw_allPortsQ).strip()
-    
-    #if/elif/else for all ports
-         
-    if allPortsQ.upper() == 'Y':
-        cmd_allPorts()
-    
-    elif allPortsQ.upper() == 'N':
-        pass()
-    
-    else:
-        print('that is not an option, please try again')
-        exit()   
 
-    #---------------------------------------------------------------------
-    raw_port_input = input(colored('and which ports would you like to scan today?[separate each port by a ","]\n >>', "white"))
-    port = str(raw_port_input).strip()
-    
+    allPorts =[f"nmap -vv -sU -p-  {site}", f"nmap -vv -sT -p-  {site}"]
+        
+    if allPortsQ == 'Y':
+        for cmd in allPorts:
+            print(f"\n\n Running: {cmd}")
+            map = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+            output, err = map.communicate()
+            allPort_Results = output.decode('ascii')
+    #       f.write(output)
+            print(output)   
+        print(f"\n\n(Complete:,{cmd}\n,{output}\n")
+        print(f"--------------------------------------")
+    #        f.write(f"\n\n(Complete:,{cmd}\n,{output}\n")
+    #        f.write(f"--------------------------------------")
+        targetPorts()
+                        
+    else: 
+        allPortsQ != 'Y'
+        print("moving to a more surgical process....")
+        targetPorts()
 
-
+        #----------------------------------------------------
     # configuration = Configuration(site, port, {"http":"http://127.0.0.1:8080","https": "http://127.0.0.1:8080"})
     configuration = Configuration(site, port)
     proxies = configuration.proxies
     cmds = configuration.cmds
     site = configuration.site
-    allPortsQ = configuration.allPorts
     r = requests.get(f"{configuration.http + configuration.site}", proxies=proxies, verify=False)
     headers = r.headers
     
-    #Opening for the log path
+
+    #---------------Opening for the log path---------------
     logpath = f"logs/{date.isoformat(date.today()).replace('-',' ')}_{site}.log"
 
     with open(logpath, 'w') as f:
-        
+
         f.write(colored("\nInitiating Recon Scan\n\n\n", "green"))
         print(colored("\nInitiating Recon Scan\n\n\n", "green"))
-        #headers Scan functions
-        
 
-
-        #Future looks like complete scan first and designate variables based on open ports
-        #Then they wouldn't have to specify ports. Research how to designate results as new variables in Python.
-        
-#-----------------Response Status Code------------------------         
+            #headers Scan functions
+            #Future looks like complete scan first and designate variables based on open ports
+            #Then they wouldn't have to specify ports. Research how to designate results as new variables in Python.
+            
+            #-----------------Response Status Code------------------------         
         f.write(format_text('Response status_code is: ',r.status_code))
         print(format_text('Response status_code is: ',r.status_code))
-        
-        #f.write(format_text('headers are: ',r.headers))/print(format_text('headers are: ',r.headers)), Only way to get them to print to document
-        
-#-----------------Header/CSP Begin------------------------        
+            
+            #-----------------Header/CSP Begin------------------------        
         f.write(text_header('The Headers returned are: '))
         print(text_header('The Headers returned are: '))
 
@@ -112,13 +116,14 @@ def main():
                 for h in head:
                     f.write(f"\t{h}")
                     print(f"\t{h}")
-#--------------------------------Header/CSP End/ Cookie Pull start--------------- 
+            #-----------Header/CSP End/ Cookie Pull start--------------- 
 
         f.write(format_text('\n\nCookies: \n',r.cookies)),
         print(format_text('\n\nCookies: \n',r.cookies)),
         f.write(format_text('HTML ',r.text)),
         print(format_text('HTML: ',r.text)),
-#-------------------------------- Cookie Pull End/CMD Start---------------       
+            
+            #----------------- Cookie Pull End/CMD Start---------------       
         for cmd in cmds:
             f.write(f"\n\n Running: {cmd}")
             map = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
@@ -131,6 +136,7 @@ def main():
             f.write(f"\n\n(Complete:,{cmd}\n,{output}\n")
             f.write(f"--------------------------------------")
         f.write(colored('**Scan Complete**','green'))
+        exit()
 #-------------------------------- Cookie Pull End/CMD Start---------------      
 
 if __name__ == "__main__":

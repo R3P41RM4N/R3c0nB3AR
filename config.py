a6a5a7e1ac065e14cd84bcb49269e9d0995cb688
R3P41RM4N <102443920+R3P41RM4N@users.noreply.github.com>
2023-02-01 21:34:17 -0700

    Update config.py
    activated all commands. will work on switching out ffuf for dirbuster or gobuster... whichever is on the application.
    I'm going to have to run this a few times to iron out the flow.
    planning on adding xsstrike tomorrow.

--- config.py
+++ config.py
@@ -15,17 +15,17 @@ class Configuration:
             f"nmap -p {self.ports} --script http-auth-finder, {site}",
             f"nmap -p {self.ports} --script http-ntlm-info {site}",
             f"nmap -p {self.ports} --script http-aspnet-debug {site}",
-            f"nmap -p {self.ports} --script http-stored-xss {site}",
+            f"nmap -p {self.ports} --script http-stored-xss {site}",#I am planning on adding XSStrike to this application and replace this one.
             f"nmap -p {self.ports} --script http-methods {site}",
             f"nmap -sS -A -sV --reason --script=http-enum {site}",
             f"nmap -p 139,145 --script=smb-enum* {site}",
             f"nmap -p 139,145 --script=smb-os-discovery.nse {site}",
             f"nmap -p {self.ports} --script=http-sitemap-generator.nse {site}",
-            #f"nikto -p {self.ports} -h {site}",
+            f"nikto -p {self.ports} -h {site}",
             f"hping3 -S {self.site} -c 15 -p {ports}", #note to research this function further
             f"curl -k {self.http}{site}/images",
             f"curl -k {self.http}{site}/Images",
             f"curl -svk {self.http}{site}/asdf",
-            #f"ffuf -w /usr/share/wordlists/SecLists/Discovery/DNS/deepmagic.com_top50kprefixes.txt -u https://{site}/FUZZ -c -v -fc 404,302,301",
-            #f"ffuf -w /usr/share/seclists/Discovery/DNS/dns-Jhaddix.txt -u http://target.com/ -H "Host:FUZZ.{site}" -of md -o subdomain/fuzzing_dnsjhaddix.md",
-        ]
+            f"ffuf -w /usr/share/wordlists/SecLists/Discovery/DNS/deepmagic.com_top50kprefixes.txt -u https://{site}/FUZZ -c -v -fc 404,302,301",
+            f"ffuf -w /usr/share/seclists/Discovery/DNS/dns-Jhaddix.txt -u http://target.com/ -H "Host:FUZZ.{site}" -of md -o subdomain/fuzzing_dnsjhaddix.md",
+        ] 

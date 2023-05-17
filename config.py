9f8beec15c6d9f83f1ebe0b60193a9f1983299d2
R3P41RM4N <102443920+R3P41RM4N@users.noreply.github.com>
2023-05-17 17:33:39 -0600

    Update config.py
    linked curl and gobuster to proxy which could then be used for authentication and provide better feedback if needed via burp suite.

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ class Configuration:
         site = self.site.split(":")[0]
 
         self.cmds =[
+            f"whatweb -a 3 --verbose {site}",
             f"nmap -Pn -sC -T4 --reason {site}",
             f"nmap -Pn -T4 --script ssl-enum-ciphers --reason {site}",
             f"nmap -p {self.ports} --script http-auth, {site}",
@@ -23,11 +24,13 @@ class Configuration:
             f"nmap -p {self.ports} --script=http-sitemap-generator.nse {site}",
             f"nikto -p {self.ports} -h {site}",
             f"hping3 -S {self.site} -c 15 -p {ports}", #note to research this function further
+            f"curl -kLv {self.http}{site} --proxy 127.0.0.1 --proxy-anyauth", #| grep hidden -A 
+            #15 -B 5 ",
             f"curl -k {self.http}{site}/images",
             f"curl -k {self.http}{site}/Images",
             f"curl -svk {self.http}{site}/asdf",
-            f"gobuster fuzz -r -w /usr/share/wordlists/SecLists/Discovery/Web_Content/common.txt -u https://{site}/FUZZ",
-            f"gobuster fuzz -r -w /usr/share/wordlists/SecLists/Discovery/Web_Content/common.txt -u https://FUZZ.{site}",
+            f"gobuster dir -r -e --url https://{site} -w /usr/share/wordlists/SecLists/Discovery/Web_Content/common.txt --proxy 127.0.0.1:8080 -k",
+            #f"gobuster fuzz -r -w /usr/share/wordlists/SecLists/Discovery/Web_Content/common.txt -u https://FUZZ.{site}",
             
             #f"ffuf -w /usr/share/wordlists/SecLists/Discovery/DNS/deepmagic.com_top50kprefixes.txt -u https://{site}/FUZZ -c -v -fc 404,302,301",
             #f"ffuf -w /usr/share/seclists/Discovery/DNS/dns-Jhaddix.txt -u http://target.com/ -H "Host:FUZZ.{site}" -of md -o subdomain/fuzzing_dnsjhaddix.md",
